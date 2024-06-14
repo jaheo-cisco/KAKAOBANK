d4a9b12ade695079bd68ee7df009ba422f093693
jaheo-cisco <jaheocisco@gmail.com>
2024-06-14 20:04:46 +0900

    Update ndfc_delete_switch_perfabric.py

--- ndfc-python/ndfc_delete_switch_perfabric.py
+++ ndfc-python/ndfc_delete_switch_perfabric.py
@@ -9,27 +9,18 @@ import sys
 import pprint
 
 
-if len(sys.argv) < 3:
-    print(f'usage python {sys.argv[0]} fabric-name switch_name')
+if len(sys.argv) < 2:
+    print(f'usage python {sys.argv[0]} fabric-name')
     sys.exit()
 fabricName = sys.argv[1]
-switchName = sys.argv[2]
-
-if switchName.lower() == 'all':
-    confirm = input(f'Do you really want to delete all the switches in the fabric {fabricName}? if yes confirm by typing "yes":')
-    if confirm.lower() != 'yes':
-        print('Exiting for not confirming!')
-        sys.exit()
-    elif confirm.lower() == 'yes':
-        print(f'Deleting all the switches in the fabric {fabricName}')
-
 
 username = ndfc_credentials.username
 password = ndfc_credentials.password
 url = 'https://' + ndfc_credentials.node_ip
 posturl = url + f'/appcenter/cisco/ndfc/api/v1/lan-fabric/rest//control/fabrics/{fabricName}'
 ndfc_token = ndfc_auth.auth(ndfc_credentials.node_ip,username,password)
-headers = {'Authorization': ndfc_token, 'Content-Type': 'application/json'}
+headers = {'Authorization':ndfc_token, 'Content-Type': 'application/json'}
+
 
 response = requests.get(posturl, verify=False, headers=headers)
 if response.status_code == 200:
@@ -42,35 +33,15 @@ response = requests.get(posturl, verify=False, headers=headers)
 if response.status_code == 200:
     output = json.loads(response.text)
 
-delete_node_list=[]
+out_list = []
 for item in output:
     out = dict(zip(item['displayHdrs'],item['displayValues']))
-    if out['Name'].lower() == switchName.lower():
-        delete_node = out['WWN']
-    elif switchName.lower() == 'all':
-        delete_node_list.append(out['WWN'])
+    print('------------------------------------')
+    print(out)
+    print('------------------------------------')
+    out_list.append(out)
 
-try:
-    if delete_node:
-        deleteurl = url + f'/appcenter/cisco/ndfc/api/v1/lan-fabric/rest//control/fabrics/{fabricName}/switches/{delete_node}'
-        response = requests.delete(deleteurl, verify=False, headers=headers)
-        if response.status_code == 200:
-            print(response.text)
-        else:
-            print('device node deleted',response.text)
-except:
-    if len(delete_node_list)>0:
-        for delete_node in delete_node_list:
-            deleteurl = url + f'/appcenter/cisco/ndfc/api/v1/lan-fabric/rest//control/fabrics/{fabricName}/switches/{delete_node}'
-            response = requests.delete(deleteurl, verify=False, headers=headers)
-            #print(response.text)
-            if response.status_code == 200:
-                print(response.text)
-            else:
-                print('device node deleted', response.text)
-
-
-
-
-
-
+# Save the entire out_list to a JSON file
+with open('out.json', 'w') as file:
+    json.dump(out_list, file)
+    
